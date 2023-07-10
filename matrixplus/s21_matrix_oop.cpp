4c0c9c6ec3776bcabd9cc7c2b480fa6ac15b6d3e
rama <i@rrmstu.ru>
2023-07-10 08:34:15 +0500

    :hammer: The constructors have been updated for easy reading. Removed unnecessary dependencies.

--- matrixplus/s21_matrix_oop.cpp
+++ matrixplus/s21_matrix_oop.cpp
@@ -1,269 +1,42 @@
 #include "s21_matrix_oop.h"
 
-/**
- * @brief Default constructor and initialize matrix with zeros (0) by default
- * @note This function is not a member of the class
- */
-S21Matrix::S21Matrix() : rows_(1), cols_(1) {
-    this->CreateMatrix(rows_, cols_);
-}
+S21Matrix::S21Matrix() : rows_(0), cols_(0), matrix_(nullptr) {}
 
-/**
- * @brief Destructor and delete matrix from memory (free memory)
- * @note This function is not a member of the class
- */
-S21Matrix::~S21Matrix() { this->DeleteMatrix(); }
-
-/**
- * @brief Allocate memory for matrix (allocate memory for rows and columns) and initialize matrix with zeros (0) by default
- * @param rows number of rows (default = 1)
- * @param cols number of columns (default = 1)
- * @note This function is not a member of the class
- */
-void S21Matrix::CreateMatrix(int rows, int cols) {
-    this->rows_ = rows;
-    this->cols_ = cols;
-    this->matrix_ = new double *[rows_];
-    for (int i = 0; i < rows_; ++i) {
-        this->matrix_[i] = new double[cols_]();
-    }
-}
-
-/**
- * @brief Delete matrix from memory (free memory) and set rows and columns to zero (0)
- * @note This function is not a member of the class
- */
-void S21Matrix::DeleteMatrix() {
-    for (int i = 0; i < rows_; ++i) {
-        delete[] this->matrix_[i];
-    }
-    delete[] this->matrix_;
-    this->rows_ = 0;
-    this->cols_ = 0;
-}
-
-/**
- * @brief Constructor with parameters (rows, cols) and initialize matrix with zeros (0) by default
- * @param rows number of rows (default = 1)
- * @param cols number of columns (default = 1)
- * @throws std::invalid_argument if matrix size is invalid (rows < 1 or cols < 1)
- * @note This function is not a member of the class
- */
 S21Matrix::S21Matrix(int rows, int cols) : rows_(rows), cols_(cols) {
-    if (rows < 1 || cols < 1) {
-        throw std::invalid_argument("Invalid matrix size");
-    } else {
-        this->CreateMatrix(rows, cols);
+  if (rows_ <= 0 || cols_ <= 0) {
+    throw std::invalid_argument(
+        "Invalid matrix dimensions. Rows and columns must be greater than 0.");
+  } else {
+    matrix_ = new double *[rows_];
+    for (int i = 0; i < rows_; ++i) {
+      matrix_[i] = new double[cols_];
     }
+  }
 }
 
-/**
- * @brief Copy constructor and copy matrix from other matrix (copy rows, cols and matrix)
- * @param other matrix to copy
- * @note This function is not a member of the class
- */
 S21Matrix::S21Matrix(const S21Matrix &other)
-        : rows_(other.rows_), cols_(other.cols_) {
-    this->CreateMatrix(other.rows_, other.cols_);
-    for (int i = 0; i < other.rows_; ++i) {
-        for (int j = 0; j < other.cols_; ++j) {
-            this->matrix_[i][j] = other.matrix_[i][j];
-        }
-    }
+    : rows_(other.rows_), cols_(other.cols_) {
+  // Allocate memory for the matrix
+  matrix_ = new double *[rows_];
+  for (int i = 0; i < rows_; ++i) {
+    matrix_[i] = new double[cols_];
+    // Copy the elements from the other matrix
+    std::copy(other.matrix_[i], other.matrix_[i] + cols_, matrix_[i]);
+  }
 }
 
-/**
- * @brief Move constructor and move matrix from other matrix (move rows, cols and matrix)
- * @param other matrix to move
- * @note This function is not a member of the class
- */
-S21Matrix::S21Matrix(S21Matrix &&other) noexcept {
-    this->rows_ = other.rows_;
-    this->cols_ = other.cols_;
-    this->matrix_ = other.matrix_;
-    other.rows_ = 0;
-    other.cols_ = 0;
-    other.matrix_ = nullptr;
+S21Matrix::S21Matrix(S21Matrix &&other)
+    : rows_(other.rows_), cols_(other.cols_), matrix_(other.matrix_) {
+  // Reset the other matrix
+  other.rows_ = 0;
+  other.cols_ = 0;
+  other.matrix_ = nullptr;
 }
 
-/**
- * @brief Compare matrix with matrix (A == B) = true (this == other) = true
- * @param other matrix to compare
- * @return true if matrix are equal, false otherwise
- * @note This function is not a member of the class
- */
-bool S21Matrix::EqMatrix(const S21Matrix &other) {
-    if (this->rows_ != other.rows_ || this->cols_ != other.cols_) {
-        return false;
-    } else {
-        for (int i = 0; i < this->rows_; ++i) {
-            for (int j = 0; j < this->cols_; ++j) {
-                if (std::abs(this->matrix_[i][j] - other.matrix_[i][j]) > EPSILON) {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
+S21Matrix::~S21Matrix() {
+  // Deallocate memory for the matrix
+  for (int i = 0; i < rows_; ++i) {
+    delete[] matrix_[i];
+  }
+  delete[] matrix_;
 }
-
-/**
- * @brief Sum matrix with matrix (A + B) = C (this + other = result)
- * @param other matrix to sum with this matrix (B)
- * @return result of summation (C)
- * @throws std::invalid_argument if matrix sizes are not equal
- * @note This function is not a member of the class
- */
-void S21Matrix::SumMatrix(const S21Matrix &other) {
-    if (this->rows_ != other.rows_ || this->cols_ != other.cols_) {
-        throw std::invalid_argument("Invalid matrix size");
-    } else {
-        for (int i = 0; i < this->rows_; ++i) {
-            for (int j = 0; j < this->cols_; ++j) {
-                this->matrix_[i][j] += other.matrix_[i][j];
-            }
-        }
-    }
-}
-
-/**
- * @brief Subtract matrix from matrix (A - B) = C (this - other = result)
- * @param other matrix to subtract from this matrix (B)
- * @return result of subtraction (C)
- * @throws std::invalid_argument if matrix sizes are not equal
- * @note This function is not a member of the class
- */
-void S21Matrix::SubMatrix(const S21Matrix &other) {
-    if (this->rows_ != other.rows_ || this->cols_ != other.cols_) {
-        throw std::invalid_argument("Invalid matrix size");
-    } else {
-        for (int i = 0; i < this->rows_; ++i) {
-            for (int j = 0; j < this->cols_; ++j) {
-                this->matrix_[i][j] -= other.matrix_[i][j];
-            }
-        }
-    }
-}
-
-/**
- * @brief Multiply matrix by number (A * number) = B (this * number = result)
- * @param number to multiply by this matrix (A)
- * @return result of multiplication (B)
- * @note This function is not a member of the class
- */
-void S21Matrix::MulNumber(const double number) {
-    for (int i = 0; i < this->rows_; ++i) {
-        for (int j = 0; j < this->cols_; ++j) {
-            this->matrix_[i][j] *= number;
-        }
-    }
-}
-
-/**
- * @brief Multiply matrix by matrix (matrix multiplication) (A * B) = C (this * other = result)
- * @param other matrix to multiply by this matrix (B)
- * @return result of multiplication (C)
- * @throw std::invalid_argument if matrix sizes are not equal
- * @note This function is not a member of the class
- */
-void S21Matrix::MulMatrix(const S21Matrix &other) {
-    if (this->cols_ != other.rows_) {
-        throw std::invalid_argument("Invalid matrix size");
-    } else {
-        S21Matrix result(this->rows_, other.cols_);
-        for (int i = 0; i < this->rows_; ++i) {
-            for (int j = 0; j < other.cols_; ++j) {
-                for (int k = 0; k < this->cols_; ++k) {
-                    result.matrix_[i][j] += this->matrix_[i][k] * other.matrix_[k][j];
-                }
-            }
-        }
-        this->DeleteMatrix();
-        this->rows_ = result.rows_;
-        this->cols_ = result.cols_;
-        this->matrix_ = result.matrix_;
-        result.rows_ = 0;
-        result.cols_ = 0;
-        result.matrix_ = nullptr;
-    }
-}
-
-/**
-* @brief Transpose matrix (A^T) = B (this^T = result)
-* @return result of transposition (B)
-* @note This function is not a member of the class
-*/
-S21Matrix S21Matrix::Transpose() {
-    S21Matrix result(this->cols_, this->rows_);
-    for (int i = 0; i < this->rows_; ++i) {
-        for (int j = 0; j < this->cols_; ++j) {
-            result.matrix_[j][i] = this->matrix_[i][j];
-        }
-    }
-    return result;
-}
-
-/**
- * @brief Calculate determinant of matrix (|A|) = det(A) (this = A) (this = result)
- * @return determinant of matrix (det(A))
- * @throw std::invalid_argument if matrix is not square (rows != cols) or matrix is empty (rows = 0 or cols = 0)
- * @note This function is not a member of the class
- */
-double S21Matrix::Determinant() {
-    if (this->rows_ != this->cols_ || this->rows_ <= 0 && this->cols_ <= 0) {
-        throw std::invalid_argument("Invalid matrix size");
-    } else {
-        double det = 0;
-        if (this->rows_ == 1) {
-            det = this->matrix_[0][0];
-        } else if (this->rows_ == 2) {
-            det = this->matrix_[0][0] * this->matrix_[1][1] - this->matrix_[0][1] * this->matrix_[1][0];
-        } else {
-            for (int i = 0; i < this->rows_; ++i) {
-                S21Matrix minor(this->rows_ - 1, this->cols_ - 1);
-                for (int j = 1; j < this->rows_; ++j) {
-                    for (int k = 0; k < this->cols_; ++k) {
-                        if (k < i) {
-                            minor.matrix_[j - 1][k] = this->matrix_[j][k];
-                        } else if (k > i) {
-                            minor.matrix_[j - 1][k - 1] = this->matrix_[j][k];
-                        }
-                    }
-                }
-                det += pow(-1, i) * this->matrix_[0][i] * minor.Determinant();
-            }
-        }
-        return det;
-    }
-}
-
-/**
- * @brief Calculate the algebraic complement of the matrix (A*) = B
- * @return algebraic complement of the matrix (B)
- * @throw std::invalid_argument if matrix is not square (rows != cols) or matrix is empty (rows = 0 or cols = 0)
- * @note This function is not a member of the class
- */
-S21Matrix S21Matrix::CalcComplements() {
-    S21Matrix result(this->rows_, this->cols_);
-    for (int i = 0; i < this->rows_; ++i) {
-        for (int j = 0; j < this->cols_; ++j) {
-            S21Matrix minor(this->rows_ - 1, this->cols_ - 1);
-            for (int k = 0; k < this->rows_; ++k) {
-                for (int l = 0; l < this->cols_; ++l) {
-                    if (k < i && l < j) {
-                        minor.matrix_[k][l] = this->matrix_[k][l];
-                    } else if (k < i && l > j) {
-                        minor.matrix_[k][l - 1] = this->matrix_[k][l];
-                    } else if (k > i && l < j) {
-                        minor.matrix_[k - 1][l] = this->matrix_[k][l];
-                    } else if (k > i && l > j) {
-                        minor.matrix_[k - 1][l - 1] = this->matrix_[k][l];
-                    }
-                }
-            }
-            result.matrix_[i][j] = pow(-1, i + j) * minor.Determinant();
-        }
-    }
-    return result;
-}
-
